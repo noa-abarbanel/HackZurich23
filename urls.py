5c9c71777c79f44984bfd5cf0fa2ec49c7f5acc8
noa-abarbanel <145234463+noa-abarbanel@users.noreply.github.com>
2023-09-17 02:03:43 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,24 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for videoconferencing project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/',views.register, name='register'),
-    path('login/',views.login_view, name='login'),
-    path('dashboard/',views.dashboard, name='dashboard'),
-    path('meeting/',views.videocall, name='meeting'),
-    path('logout/',views.logout_view, name='logout'),
-    path('join/',views.join_room, name='join_room'),
-    path('',views.index, name='index'),
-
+    path('admin/', admin.site.urls),
+    path('', include('appName.urls')
+         )
 ]
-
